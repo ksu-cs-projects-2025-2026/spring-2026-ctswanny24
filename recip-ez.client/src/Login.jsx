2d873641b6016bd5bcaf259e34f44822dc85e92f
Caden Swanson <ctswanny24@gmail.com>
2026-03-11 11:01:40 -0500

    Login Component UI Element Added w/ Components

---
+++ recip-ez.client/src/Login.jsx
@@ -0,0 +1,55 @@
+import { Button } from "./components/ui/button"
+import {
+    Card,
+    CardHeader,
+    CardTitle,
+    CardDescription,
+    CardContent,
+    CardFooter,
+} from "./components/ui/card"
+
+function Login() {
+    return (
+        <Card>
+            <CardHeader>
+                <CardTitle>Login to your account</CardTitle>
+                <CardDescription>Enter your email below to login to your account</CardDescription>
+            </CardHeader>
+
+            <CardContent>
+                <form>
+                    <div className="flex-col gap-6">
+                        <div>
+                            <label>Email</label>
+                            <input
+                                id="email"
+                                type="email"
+                                placeholder="abc@example.com"
+                                required
+                            />
+                        </div>
+                        <div>
+                            <label>Password</label>
+                            <input
+                                id="password"
+                                type="password"
+                                required
+                            />
+                        </div>
+                    </div>
+                </form>
+            </CardContent>
+            <CardFooter className="flex-col gap-2">
+                <Button type="submit" className="w-full">
+                    Login
+                </Button>
+                <Button variant="outline" className="w-full">
+                    Login with Google
+                </Button>
+            </CardFooter>
+        </Card>
+    );
+
+}
+
+export default Login
